2d651c417ebd178a427bfe7531e95329ab50419c
gversmee <34459351+gversmee@users.noreply.github.com>
2018-02-22 12:07:43 -0500

    suppress ENONTER warning

--- R/result.ID.R
+++ R/result.ID.R
@@ -8,7 +8,7 @@ result.ID <- function(env, body, token, verbose = FALSE) {
   if (is.null(ID))  {
     body <- gsub("ENCOUNTER", "ENOUNTER", body)
     ID <- httr::content(httr::POST(paste0(env, "/rest/v1/queryService/runQuery"), body = body))$resultId
-    if (!is.null(ID))  message(paste("results OK, but needed to change ENCONTER to ENOUNTER", env))
+    if (!is.null(ID))  message(paste("results OK", env))
   }
 
   return(ID)
