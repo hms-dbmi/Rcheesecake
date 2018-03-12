068df2d0472380ff41037f6628382088de07b7b9
gversmee <34459351+gversmee@users.noreply.github.com>
2018-03-12 12:37:42 -0400

    debug

--- R/get.result.R
+++ R/get.result.R
@@ -3,14 +3,14 @@
 get.result <- function(env, resultID, token, verbose = FALSE) {
 
 
-  if (verbose == TRUE)  message("\nDownloading the data frame")
+  if (verbose)  message("\nDownloading the data frame")
 
-  df <- content.get(paste0(env, "/rest/v1/resultService/result/", resultID, "/CSV"), token)
+  df <- suppressMessages(content.get(paste0(env, "/rest/v1/resultService/result/", resultID, "/CSV"), token))
 
-  if (!is.na(df$errorType))  {
+  if (!is.null(df$errorType))  {
     stop("Internal server error, please check with the developpers\nProcess stopped", call. = FALSE)
   } else {
-    suppressMessages(data.frame(df), check.names = FALSE)
+    data.frame(df, check.names = FALSE)
   }
 
 }
