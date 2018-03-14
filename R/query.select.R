dd2a43506b42edfaa23a603d862bf1ab5f350baa
gversmee <34459351+gversmee@users.noreply.github.com>
2018-03-13 22:53:47 -0400

    better

--- R/query.select.R
+++ R/query.select.R
@@ -5,13 +5,6 @@ query.select <- function(pathlist, verbose = FALSE)  {
   if (verbose)  message('\nBuilding the "select" part of the query')
 
   return(paste0('{"select": [',
-
-                   paste0('{"field": {"pui": "',
-                          pathlist,
-                          '","dataType": "STRING"},"alias": "',
-                          pathlist,
-                          '"}', collapse = ","),
-
-                   '],'))
-
+                paste0('{"field": {"pui": "', pathlist,'","dataType": "STRING"},"alias": "',pathlist,'"}', collapse = ","),
+                '],'))
 }
